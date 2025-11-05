6fb64e15d1c344dd1d1950aad50f4b2b70fc573f
anasilva430 <ana.silva.430@aemlaranjeira.pt>
2025-11-05 11:59:43 +0000

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- game.js
+++ game.js
@@ -857,7 +857,7 @@ function sendResults() {
     };
     
     
-    emailjs.send('service_wi13tln', 'template_km9qrq9', templateParams)
+    emailjs.send('service_wi13tin', 'template_km9qrq9', templateParams)
         .then(function(response) {
             alert('Resultado submetido com sucesso! Código: ' + response.status);
             
@@ -874,5 +874,6 @@ function sendResults() {
     // ATENÇÃO: NENHUM CÓDIGO FORA DESTA FUNÇÃO (E FORA DO } DESTE BLOCO) DEVE EXISTIR NO FIM DO FICHEIRO.
 
 }
+
 
 
